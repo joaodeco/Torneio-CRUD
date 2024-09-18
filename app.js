aa9258f1c4ef69d613cac40bbf140ccac33f7efc
Gabriellazarine <gabriellazarinesilva@gmail>
2024-09-18 17:32:11 -0300

    sim

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const app = express();
 app.use(express.json());
 
 mongoose
-    .connect("mongodb://localhost:27017/Crud Jogos").then(() => console.log("Conectado ao MongoDB")).catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
+    .connect("mongodb://localhost:27017/CrudJogos")
+    .then(() => console.log("Conectado ao MongoDB"))
+    .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro))
 
-    const atualizarJogo = require('./Jogo/atualizarJogo');
-    app.put('/', atualizarJogo);
-    
+const atualizarJogo = require('./atualizarJogo');
+app.put('/', atualizarJogo);
+
+const deletarJogo = require('./deletar');
+app.delete('/', deletarJogo);
 
 const esquemaJogo = new mongoose.Schema({
     nome: { type: String, required: true },
     ano: { type: String, required: true },
     genero: { type: String, required: true },
-    });
+});
 
 const Jogo = mongoose.model("Jogo", esquemaJogo);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
--- atualizar.js
+++ atualizar.js
@@ -1,41 +1,40 @@
-async function atualizarJogo(id, nome, genero, ano)
-{
-try {
-const jogoAtualizado = await Livro.findByIdAndUpdate(
-id,
-{ nome, genero, ano},
-{ new: true, runValidators: true }
-);
-return jogoAtualizado;
-} catch (erro) {
-console.error("Erro ao atualizar jogo", erro);
-throw erro;
-}
+async function atualizarJogo(id, nome, genero, ano) {
+    try {
+        const jogoAtualizado = await Livro.findByIdAndUpdate(
+            id,
+            { nome, genero, ano },
+            { new: true, runValidators: true }
+        );
+        return jogoAtualizado;
+    } catch (erro) {
+        console.error("Erro ao atualizar jogo", erro);
+        throw erro;
+    }
 }
 
 app.put("/jogos/:id", async (req, res) => {
     try {
-    const { id } = req.params;
-    const { nome, genero, ano } = req.body;
-    const jogoAtualizado = await atualizarJogo(
-    id,
-    nome,
-    genero,
-    ano
-    );
-    if (jogoAtualizado) {
-    res
-    .status(200)
-    .json({
-    mensagem: "Jogo atualizado com sucesso",
-    jogo: jogoAtualizado,
-    });
-    } else {
-    res.status(404).json({ mensagem: "Jogo não encontrado" });
-    }
+        const { id } = req.params;
+        const { nome, genero, ano } = req.body;
+        const jogoAtualizado = await atualizarJogo(
+            id,
+            nome,
+            genero,
+            ano
+        );
+        if (jogoAtualizado) {
+            res
+                .status(200)
+                .json({
+                    mensagem: "Jogo atualizado com sucesso",
+                    jogo: jogoAtualizado,
+                });
+        } else {
+            res.status(404).json({ mensagem: "Jogo não encontrado" });
+        }
     } catch (erro) {
-    res
-    .status(500)
-    .json({ mensagem: "Erro ao atualizar jogo", erro: erro.message });
+        res
+            .status(500)
+            .json({ mensagem: "Erro ao atualizar jogo", erro: erro.message });
     }
-    });
+});
