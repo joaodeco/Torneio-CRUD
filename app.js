f60afb2d6acdbba68df577cca9e765aa08da169a
joaodeco <murilomergner@gmail.com>
2024-09-18 15:24:12 -0300

    12

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 
 mongoose
-    .connect("mongodb://localhost:27017/TCHOVÊ")
+    .connect("mongodb://localhost:27017/Crud Jogos")
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
+
+const esquemaJogo = new mongoose.Schema({
+    nome: { type: String, required: true },
+    ano: { type: String, required: true },
+    genero: { type: String, required: true },
+    });
+
+const Jogo = mongoose.model("Jogo", esquemaJogo);
