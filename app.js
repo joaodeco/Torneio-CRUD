a890641a7669af5e81e47d7e870d6a003d610a22
ConradoFrasson <conradox12@gmail.com>
2024-09-18 16:20:00 -0300

    12

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.json());
 mongoose
     .connect("mongodb://localhost:27017/Crud Jogos").then(() => console.log("Conectado ao MongoDB")).catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
 
+    const atualizarJogo = require('./Jogo/atualizarJogo');
+    app.put('/', atualizarJogo);
+    
+
 const esquemaJogo = new mongoose.Schema({
     nome: { type: String, required: true },
     ano: { type: String, required: true },
---
+++ atualizar.js
@@ -0,0 +1,41 @@
+async function atualizarJogo(id, nome, genero, ano)
+{
+try {
+const jogoAtualizado = await Livro.findByIdAndUpdate(
+id,
+{ nome, genero, ano},
+{ new: true, runValidators: true }
+);
+return jogoAtualizado;
+} catch (erro) {
+console.error("Erro ao atualizar jogo", erro);
+throw erro;
+}
+}
+
+app.put("/jogos/:id", async (req, res) => {
+    try {
+    const { id } = req.params;
+    const { nome, genero, ano } = req.body;
+    const jogoAtualizado = await atualizarJogo(
+    id,
+    nome,
+    genero,
+    ano
+    );
+    if (jogoAtualizado) {
+    res
+    .status(200)
+    .json({
+    mensagem: "Jogo atualizado com sucesso",
+    jogo: jogoAtualizado,
+    });
+    } else {
+    res.status(404).json({ mensagem: "Jogo não encontrado" });
+    }
+    } catch (erro) {
+    res
+    .status(500)
+    .json({ mensagem: "Erro ao atualizar jogo", erro: erro.message });
+    }
+    });
