945f7c645e399b39b9c0c960725157c0d5950050
Gabriellazarine <gabriellazarinesilva@gmail>
2024-09-19 16:00:01 -0300

    Final

--- adicionar.js
+++ adicionar.js
@@ -1,6 +1,9 @@
+
+const Jogo = require('./esquema')
+
 async function adicionarJogo(nome, ano, genero){
     try{
-        const novoJogo = new Jogo({nome, ano, genero})
+        const novoJogo = new Jogo ({nome, ano, genero})
         return await novoJogo.save()
     }catch(erro) {
         console.log(`Deu erro para criar o jogo: ${erro}`)
--- deletar.js
+++ deletar.js
@@ -1,3 +1,5 @@
+const Jogo = require('./esquema')
+
 async function deletarJogo(id) {
     try {
         const jogoDeletado = await Jogo.findByIdAndDelete(id);
@@ -8,20 +10,4 @@ async function deletarJogo(id) {
     }
 }
 
-app.delete("/jogos/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const jogoDeletado = await deletarJogo(id);
-        if (jogoDeletado) {
-            res
-                .status(200)
-                .json({ mensagem: "Jogo deletado com sucesso", jogo: jogoDeletado });
-        } else {
-            res.status(404).json({ mensagem: "Jogo não encontrado" });
-        }
-    } catch (erro) {
-        res
-            .status(500)
-            .json({ mensagem: "Erro ao deletar jogo", erro: erro.message });
-    }
-});
+module.exports = deletarJogo
